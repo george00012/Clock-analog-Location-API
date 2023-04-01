d87b9f7eb4aa1fe9a4f4437232c74cf3cda7b133
george00012 <georgenarcis12@yahoo.es>
2023-04-01 14:24:06 +0200

    base

---
+++ app.js
@@ -0,0 +1,19 @@
+
+/*RELOJ*/ 
+const horas = document.querySelector(".principal-clock-hours");
+const minutos = document.querySelector(".principal-clock-min");
+const segundos = document.querySelector(".principal-clock-sec");
+
+
+
+
+
+
+
+
+
+
+
+
+/*FECHA*/
+
